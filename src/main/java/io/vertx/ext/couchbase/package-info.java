57d475178c639cb669c2d2e2090705f27f3eeaa4
syklevin <levin@alliance.hk>
2015-05-08 20:45:30 +0800

    update couchbase lib and bump to 3.1.3

--- src/main/java/io/vertx/ext/couchbase/package-info.java
+++ src/main/java/io/vertx/ext/couchbase/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Created by levin on 1/19/2015.
  */
-@GenModule(name = "vertx-couchbase")
+@GenModule(name = "vertx-couchbase", groupPackageName = "io.vertx.ext.couchbase")
 package io.vertx.ext.couchbase;
 
 import io.vertx.codegen.annotations.GenModule;
